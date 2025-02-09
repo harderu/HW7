0483b805cf4e0538bb6cc0a63ac95e26a0766af8
harderu <live.imho@gmail.com>
2025-02-10 00:03:59 +0300

    stop

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,11 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        for (int id = 2; id <= 100; id += 2) {
+        int id = 2;
+        while (id <= 100) {
             System.out.println("Уволен айтишник с id" + " " + id);
-
-
+            id += 2;
         }
-
     }
 }
